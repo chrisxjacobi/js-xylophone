ea13bff6d9ed185c04f1794af2a2dce942f5a0d7
chrisxjacobi <chrisxjacobi@gmail.com>
2017-12-09 17:33:21 -0600

    changed sound files to those more resembling a xylophone, added a note for c-low and c-high

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # js-xylophone
-A rollover JavaScript app that plays the notes of a xylophone
+A rollover JavaScript app that plays the notes of a xylophone!
--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
-    var audioC = document.getElementById("c-audio");
+    var audioCLow = document.getElementById("c-low-audio");
     var audioD = document.getElementById("d-audio");
     var audioE = document.getElementById("e-audio");
     var audioF = document.getElementById("f-audio");
     var audioG = document.getElementById("g-audio");
     var audioA = document.getElementById("a-audio");
     var audioB = document.getElementById("b-audio");
+    var audioCHigh = document.getElementById("c-high-audio");
 
-    $('#c').mouseover(function () {
-        audioC.currentTime = 0;
-        audioC.play();
+    $('#c-low').mouseover(function () {
+        audioCLow.currentTime = 0;
+        audioCLow.play();
     });
 
     
@@ -41,6 +42,11 @@ $(document).ready(function () {
     $('#b').mouseover(function () {
         audioB.currentTime = 0;
         audioB.play();
+    });
+    
+    $('#c-high').mouseover(function () {
+        audioCHigh.currentTime = 0;
+        audioCHigh.play();
     });
 
 
